bc856f8a4743a737b9e0ee8dd21c7729d75d6a0d
thekharche <ashishkharchexia@gmail.com>
2023-05-08 18:43:08 +0100

    Maestro Bitrise iOS

---
+++ maestro-bitrise-ios/maestro_bitrise_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  maestro_bitrise_iosApp.swift
+//  maestro-bitrise-ios
+//
+//  Created by ask on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct maestro_bitrise_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
